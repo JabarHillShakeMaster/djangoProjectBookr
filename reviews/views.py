dca10a14a8c01a4e511aae9a793bba3d6aba2522
Shake <majshake80@gmail.com>
2024-06-15 20:04:14 -0500

    18th commit, 1st of the day, 18_01v_2024-06-15

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Review
+from .models import Book, Review, Contributor
 from .utils import average_rating
 from django.db import models
+from .forms import SearchForm
 
 def index(request):
     # name = "world"
@@ -17,21 +18,54 @@ def index(request):
     # return HttpResponse(name)
     return render(request, "base.html")
 
-def searchResult(request):
-    search = request.GET.get("search") or "</html>"
+"""def searchResult(request):
+    search_text = request.GET.get("search", "")
+    form = SearchForm(request.GET)
+    books = set()
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.clearn_data.get("search_in") or "title"
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        else:
+            fname_contributors = Contributor.objects.filter(first_names__icontains=search)
+            for contributors in fname_contributors:
+                for book in contributors.book_set.all():
+                    books.add(book)
+            lname_contributors = Contributor.objects.filter(last_name__icontrains=search)
+            for contributors in lname_contributors:
+                for book in contributors.book_set.all():
+                    books.add(book)
 
-    return render(request, "searchResult.html", {"search": search})
+    return render(request, "search-result.html", {"form": form, "search_text": search_text, "books": books})
 
-
-"""def welcome_view(request):
+def welcome_view(request):
     message = f"<html><h1>Welcome to Bookr!</h1> \
               <p>{Book.objects.count()} books and counting!</p></html>"
               
     return render(request, 'base.html')#HttpResponse(message)
 """
 
-def book_search():
-    return render()
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    form = SearchForm(request.GET)
+    books = set()
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in") or "title"
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        else:
+            fname_contributors = Contributor.objects.filter(first_names__icontains=search)
+            for contributors in fname_contributors:
+                for book in contributors.book_set.all():
+                    books.add(book)
+            lname_contributors = Contributor.objects.filter(last_name__icontrains=search)
+            for contributors in lname_contributors:
+                for book in contributors.book_set.all():
+                    books.add(book)
+
+    return render(request, "reviews/search-results.html", {"form": form, "search_text": search_text, "books": books})
 
 def book_list(request):
     books = Book.objects.all()
